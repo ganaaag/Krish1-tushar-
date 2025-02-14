d65f05ef086170924296405674ad222503a85a46
Tushar <tusartxt7570@gmail.com>
2025-02-14 14:01:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_NAME = "Luminatbot"
-API_ID    = 21567814
-API_HASH  = "cd7dc5431d449fd795683c550d7bfb7e"
-BOT_TOKEN = "7662813243:AAEA3yIU_BZTCHR-lEXRFBelPfDdubYuRFA"
+API_ID    = 24473318
+API_HASH  = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
+BOT_TOKEN = "7791956776:AAHVK6CthoGj3DLNxQ1ojzWnUeDK-Y7CLKA"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
