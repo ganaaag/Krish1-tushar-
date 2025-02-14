6c69a61d2908be15295da50986276b14b6eb449f
Tushar <tusartxt7570@gmail.com>
2025-02-14 14:03:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ API_ID    = 24473318
 API_HASH  = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
 BOT_TOKEN = "7791956776:AAHVK6CthoGj3DLNxQ1ojzWnUeDK-Y7CLKA"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+MONGO_URI = "mongodb+srv://tusartxt7570:K1AVnnJlDceZBiRY>@cluster0.78fhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
