49fb7549c69ba1aab208a06f5c33a7aa50bbfce9
Abhishek972161 <abhishek972161@gmail.com>
2025-02-15 21:33:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_NAME = "Luminatbot"
-API_ID    = 24473318
+API_ID    = "24473318"
 API_HASH  = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
 BOT_TOKEN = "7791956776:AAHVK6CthoGj3DLNxQ1ojzWnUeDK-Y7CLKA"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
