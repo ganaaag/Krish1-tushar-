4e187d43b034bc7ed386d297b4e13aec461eafb1
ganaaag <127759334+ganaaag@users.noreply.github.com>
2025-05-04 08:58:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ API_HASH  = "d700b4abe3d023165894291b6fcbd3a0"
 BOT_TOKEN = "7585830486:AAEIfqrVUoX9sSVGXaO0ICVhbQVh7EuOsyw"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
 MONGO_URI = "mongodb+srv://mewadonlinestudy:yYjxKiZGQag8DnWH@cpvod.ywpecvo.mongodb.net/?retryWrites=true&w=majority&appName=cpvodak"
+# config.py
+authorized_users = [1081310808]
